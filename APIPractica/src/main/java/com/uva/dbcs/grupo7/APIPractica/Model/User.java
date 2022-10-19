b9831e23fecc482c9f6959dbe6fa29e2ce6cde2b
Motro <motro7@hotmail.com>
2022-10-19 18:15:00 +0200

    Modelo User provisional

--- APIPractica/src/main/java/com/uva/dbcs/grupo7/APIPractica/Model/User.java
+++ APIPractica/src/main/java/com/uva/dbcs/grupo7/APIPractica/Model/User.java
@@ -1,5 +1,129 @@
 package com.uva.dbcs.grupo7.APIPractica.Model;
 
+import java.util.Date;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Table(name = "User")
 public class User {
-    int id;
+
+    @Id
+    @GeneratedValue
+    Integer id;
+    @Size(max = 25)
+    private String name;
+    @Size(max = 25)
+    private String firstName;
+    @Size(max = 25)
+    private String lastName;
+    @Basic(optional = false)
+    @Column(unique = true)
+    private String email;
+    @Basic(optional = false)
+    private String password;
+    private Boolean enabled;
+    private String role;
+    private Date createdAt;
+    private Date updatedAt;
+
+    User() {
+
+    }
+
+    User(String name, String firstName, String lastName, String email, String password, Boolean enabled, String role,
+            Date createdAt, Date updatedAt) {
+        this.name = name;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.enabled = enabled;
+        this.role = role;
+        this.createdAt = new Date(System.currentTimeMillis());
+        // updated
+
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return this.enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
 }
