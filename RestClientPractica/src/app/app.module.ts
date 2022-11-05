c288a93269879bd6a78b45097932dc26996a9766
Jorge Rebé <jorge.rebe@alumnos.uva.es>
2022-11-05 12:32:51 +0100

    arreglo estructura toastservice

--- RestClientPractica/src/app/app.module.ts
+++ RestClientPractica/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CrearUserComponent } from './user-crear/user-crear.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ToastsContainer } from './shared/toasts-container.component';
+import { ToastService } from './shared/toast-service';
 
 
 
@@ -34,7 +35,7 @@ import { ToastsContainer } from './shared/toasts-container.component';
   ],
   providers: [
     ClienteApiRestService,
-    DataService],
+    ToastService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
