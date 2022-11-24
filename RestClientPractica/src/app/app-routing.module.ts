bbd033657a8be12bc0a94f30be1d69765991ea6a
Motro <motro7@hotmail.com>
2022-11-24 16:50:22 +0100

    login y prohibicion de rutas hecho

--- RestClientPractica/src/app/app-routing.module.ts
+++ RestClientPractica/src/app/app-routing.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { UserListarComponent } from './user-listar/user-listar.component';
 import { EditarUserComponent } from './user-editar/user-editar.component';
 import { CrearUserComponent } from './user-crear/user-crear.component';
+import { LoginGuard } from './shared/login/login.guard';
 
 const routes: Routes = [
-  {path: 'users', component:UserListarComponent, title:'Gestor Usuarios'},
-  {path: 'users/:id/editar', component:EditarUserComponent, title:'Editar usuario'},
-  {path: 'users/nuevo', component:CrearUserComponent, title:'Añadir usuario'},
-  {path: '**', redirectTo:'users',pathMatch:'full'}
+  { path: 'login', component: LoginComponent, title: 'Login' },
+  {
+    path: 'users',
+    component: UserListarComponent,
+    title: 'Gestor Usuarios',
+    canActivate: [LoginGuard],
+  },
+  {
+    path: 'users/:id/editar',
+    component: EditarUserComponent,
+    title: 'Editar usuario',
+    canActivate: [LoginGuard],
+  },
+  {
+    path: 'users/nuevo',
+    component: CrearUserComponent,
+    title: 'Añadir usuario',
+    canActivate: [LoginGuard],
+  },
+  {
+    path: '**',
+    redirectTo: 'users',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
