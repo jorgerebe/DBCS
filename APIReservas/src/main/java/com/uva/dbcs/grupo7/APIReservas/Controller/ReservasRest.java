941a665bc1d7074eb8aff818926087b2ef2f0de4
Jorge Rebé <jorge.rebe@alumnos.uva.es>
2022-12-18 19:56:05 +0100

    Avances - dockercompose, APIReservas

--- APIReservas/src/main/java/com/uva/dbcs/grupo7/APIReservas/Controller/ReservasRest.java
+++ APIReservas/src/main/java/com/uva/dbcs/grupo7/APIReservas/Controller/ReservasRest.java
@@ -1,5 +1,6 @@
 package com.uva.dbcs.grupo7.APIReservas.Controller;
 
+import java.util.Base64;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.uva.dbcs.grupo7.APIReservas.Exception.ReservaException;
 import com.uva.dbcs.grupo7.APIReservas.Model.DateRange;
 import com.uva.dbcs.grupo7.APIReservas.Model.Reserva;
+import com.uva.dbcs.grupo7.APIReservas.Model.Token;
 import com.uva.dbcs.grupo7.APIReservas.Repository.ReservaRepository;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.json.JSONObject;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,23 +36,39 @@ public class ReservasRest {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String newReserva(@RequestBody Reserva newReserva, HttpServletResponse response) {
+    public String newReserva(@RequestHeader ("authorization") String tokenHeader, @RequestBody Reserva newReserva, HttpServletResponse response) {
+
+        Token token = getToken(tokenHeader);
+        if(!token.isGuest()){
+            throw new ReservaException("No autorizado");
+        }
+
         response.setHeader("Content-type", "application/json");
         if(rooms - repository.findAllByDateInGreaterThanEqualAndDateOutLessThanEqual(newReserva.getDateIn(),newReserva.getDateOut()).size() < newReserva.getUnits()){
             throw new ReservaException("No quedan habitaciones suficientes para realizar esta reserva.");
         }
 
-        
-
-
-            repository.save(newReserva);
-            return "Nuevo registro creado";
+        repository.save(newReserva);
+        response.setStatus(200);
+        return "Nuevo registro creado";
 
     }
     @GetMapping
-    public List<Reserva> getReservas() {
+    public List<Reserva> getReservas(@RequestHeader ("authorization") String tokenHeader) {
 
-        List<Reserva> ReservaList = repository.findAll();
+        List<Reserva> ReservaList;
+
+        Token token = getToken(tokenHeader);
+        if(token.isGuest()){
+            ReservaList = repository.findByGuestID(token.getId());
+        }
+        else if(token.isHost()){
+            ReservaList = repository.findAll(Sort.by(Sort.Direction.ASC, "created_at"));
+        }
+        else{
+            throw new ReservaException("No autorizado");
+        }
+
         return ReservaList;
     }
 
@@ -62,7 +79,8 @@ public class ReservasRest {
     }
 
     @GetMapping(value = "/availability")
-    public List<Reserva> getAvailability(@RequestBody DateRange dates) {
+    public List<Reserva> getAvailability(@RequestHeader ("authorization") String tokenHeader, @RequestBody DateRange dates) {
+
         if(dates.getDateIn().isAfter(dates.getDateOut())){
             throw new ReservaException("La fecha inicial tiene que ser anterior a la final.");
         }
@@ -71,17 +89,28 @@ public class ReservasRest {
     }
 
     @GetMapping(value = { "/{id}" })
-    public Reserva getReservaPorId(@PathVariable Integer id) {
+    public Reserva getReservaPorId(@RequestHeader ("authorization") String tokenHeader, @PathVariable Integer id) {
+
+        Token token = getToken(tokenHeader);
+        if(!token.isHost()){
+            throw new ReservaException("No autorizado");
+        }
+
         Reserva reserva = repository.findById(id)
                 .orElseThrow(() -> new ReservaException("No se ha encontrado la reserva con id: " + id + "."));
         return reserva;
     }
 
     @PutMapping("/{id}")
-    public String updateUser(@PathVariable Integer id, @RequestBody Reserva res) {
+    public String updateBook(@RequestHeader ("authorization") String tokenHeader, @PathVariable Integer id, @RequestBody Reserva res) {
+
+        Token token = getToken(tokenHeader);
+        if(!token.isHost()){
+            throw new ReservaException("No autorizado");
+        }
+
         Reserva existente = repository.findById(id)
                 .orElseThrow(() -> new ReservaException("No se ha encontrado la reserva con id: " + id + "."));
-
 
 
         existente.setGuestName(res.getGuestName());
@@ -101,4 +130,26 @@ public class ReservasRest {
 
         return "Cambios realizados.";
     }
+
+    private Token getToken(String tokenPre){
+
+        String token = tokenPre.substring(7);
+
+        System.out.println(token);
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+
+
+        String[] chunks = token.split("\\.");
+        String payload = new String(decoder.decode(chunks[1]));
+
+        JSONObject json = new JSONObject(payload);
+        String role = json.getString("role");
+        String name = json.getString("name");
+        int id = json.getInt("id");
+
+        Token tokenConstruido = new Token(name, id, role);
+
+        return tokenConstruido;
+    }
+    
 }
